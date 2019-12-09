383fe564cfb00bb0eb54047dcf9da7319c9fe1d2
jianxin <jianxin@intbee.com>
2019-12-09 10:39:06 +0800

    test

---
+++ web/web.py
@@ -0,0 +1,17 @@
+from tornado import web,ioloop,httpserver
+
+class MainPageHandler(web.RequestHandler):
+    def get(self, **arg, **kwargs):
+        self.render("index.html")
+
+application = web.Application([
+            (r"/", MainPageHandler),
+    ])
+
+def main():
+    http_server = httpserver.HTTPServer(application)
+    http_server.listen(8080)
+    ioloop.IOLoop.current().strat()
+
+if __name__ == '__mian__':
+    main()
